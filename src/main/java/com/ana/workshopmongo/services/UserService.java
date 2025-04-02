5d67604c9bcfa3a39cd7f51f9285171b7e5900a3
acp-athena <anacpaula83@gmail.com>
2025-04-02 15:04:48 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/ana/workshopmongo/services/UserService.java
+++ src/main/java/com/ana/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ana.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,18 +22,19 @@ public class UserService {
 	}
 	
 	public User findById(String id) {
-		User user = repo.findOne(id);
-		if(user == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado!");
-		}
-		return user;
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
 	
-
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
