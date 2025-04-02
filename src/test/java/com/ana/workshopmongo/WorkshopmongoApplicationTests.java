c7132cd444871cf7c867c12e07252ee617d1b931
acp-athena <anacpaula83@gmail.com>
2025-04-02 12:00:23 -0300

    Primeiro commit - Projeto Criado

---
+++ src/test/java/com/ana/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ana.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
