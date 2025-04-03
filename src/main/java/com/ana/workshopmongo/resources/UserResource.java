0a441fa7deae4c25509ad349e1a1150759539973
acp-athena <anacpaula83@gmail.com>
2025-04-03 09:36:34 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/ana/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ana/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ana.workshopmongo.domain.Post;
 import com.ana.workshopmongo.domain.User;
 import com.ana.workshopmongo.dto.UserDTO;
 import com.ana.workshopmongo.services.UserService;
@@ -63,5 +64,10 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 	
 }
